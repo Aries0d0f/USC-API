5746d18dc5b33397eab2854900b508f4dea6234a
Aries Cs <aries0d0f@gmail.com>
2018-02-23 09:32:43 +0800

    Add CORS rules.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const app            = new (require('koa'))()
 const api            = new (require('koa-router'))()
+const cors           = require('@koa/cors')
 
 const classTable     = require('./model/classTable')
 
+app.use(cors())
 app.use(api.routes())
 api.get('/:id/:year/:sem', async ctx => {
   ctx.body = await classTable(ctx.params, ctx)
